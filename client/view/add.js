7dcbf90d019ef5b7e3b19bfd15ae3d86303af8b9
Matthew Bunday <zenceph@gmail.com>
2014-11-09 16:21:33 -0500

    Finish form submit handler

--- client/view/add.js
+++ client/view/add.js
@@ -2,4 +2,9 @@ Template.add.events({
   'submit form': function(event) {
     event.preventDefault();
     var email = $(event.target).children('input').eq(0).val();
+    Meteor.call('addInvite', email, function (error, result) {
+      console.log(result);
+    });
+    event.target.reset();
+  }
 })
