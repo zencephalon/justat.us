5b82c22e16f8139b2c348d3a1a3c1d78c710650b
Matthew Bunday <zenceph@gmail.com>
2014-11-12 16:40:20 -0500

    Add facet permissions.

--- model/facet.js
+++ model/facet.js
@@ -1,5 +1,21 @@
 Facets = new Meteor.Collection("facets");
 
+Facets.allow({
+  insert: function (userId, doc) {
+    // the user must be logged in, and the document must be owned by the user
+    return (userId && doc.uid === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.uid === userId;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.uid === userId;
+  },
+  fetch: ['uid']
+});
+
 Facet = Model(Facets);
 
 Facet.findOne = function(id) {
