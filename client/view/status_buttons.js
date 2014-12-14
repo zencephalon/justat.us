11630c848a51194639b1d41931b38a7d558ff942
Matthew Bunday <zenceph@gmail.com>
2014-12-14 03:25:01 -0500

    Refactor to select on focus.

--- client/view/status_buttons.js
+++ client/view/status_buttons.js
@@ -3,30 +3,29 @@ Template.status_buttons.events({
     u = User.current();
     u.setStatus({color: 'red'})
     $('input[name=status_text]').focus();
-    $('input[name=status_text]').select();
   },
   'click #status-yellow': function(event) {
     u = User.current();
     u.setStatus({color: 'yellow'})
     $('input[name=status_text]').focus();
-    $('input[name=status_text]').select();
   },
   'click #status-green': function(event) {
     u = User.current();
     u.setStatus({color: 'green'})
     $('input[name=status_text]').focus();
-    $('input[name=status_text]').select();
   },
   'click #status-blue': function(event) {
     u = User.current();
     u.setStatus({color: 'blue'})
     $('input[name=status_text]').focus();
-    $('input[name=status_text]').select();
   },
   'submit #user_status': function(event) {
     console.log("HELLO");
     event.preventDefault();
     User.current().setStatus({text: $(event.target).children('input').val()});
+  },
+  'focus input[name=status_text]': function(event) {
+    $(event.target).select();
   },
   'blur input[name=status_text]': function(event) {
     User.current().setStatus({text: $(event.target).val()});
