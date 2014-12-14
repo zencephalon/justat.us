9658f181a8418ddbc36dc571e0929e0c6e22a5a5
Matthew Bunday <zenceph@gmail.com>
2014-12-14 03:22:26 -0500

    Select status text on status change

--- client/view/status_buttons.js
+++ client/view/status_buttons.js
@@ -1,19 +1,27 @@
 Template.status_buttons.events({
   'click #status-red': function(event) {
     u = User.current();
-    u.setStatus({color: 'red', text: 'Concentrating, sorry.'})
+    u.setStatus({color: 'red'})
+    $('input[name=status_text]').focus();
+    $('input[name=status_text]').select();
   },
   'click #status-yellow': function(event) {
     u = User.current();
-    u.setStatus({color: 'yellow', text: 'Reading...'})
+    u.setStatus({color: 'yellow'})
+    $('input[name=status_text]').focus();
+    $('input[name=status_text]').select();
   },
   'click #status-green': function(event) {
     u = User.current();
-    u.setStatus({color: 'green', text: 'Just chilling.'})
+    u.setStatus({color: 'green'})
+    $('input[name=status_text]').focus();
+    $('input[name=status_text]').select();
   },
   'click #status-blue': function(event) {
     u = User.current();
-    u.setStatus({color: 'blue', text: "Let's make plans!"})
+    u.setStatus({color: 'blue'})
+    $('input[name=status_text]').focus();
+    $('input[name=status_text]').select();
   },
   'submit #user_status': function(event) {
     console.log("HELLO");
