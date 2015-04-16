d3b0be9acc35503be971bbb9b72b0052406acce9
Matthew Bunday <zenceph@gmail.com>
2015-04-16 16:54:43 -0400

    Enable flowtype

---
+++ client/view/layout.js
@@ -0,0 +1,10 @@
+Template.layout.rendered = function() {
+  $('body').flowtype({
+    minimum: 0,
+    maximum: 1600,
+    minFont: 12,
+    maxFont: 42,
+    fontRatio: 40,
+    lineRatio: 1.45
+  });
+}
